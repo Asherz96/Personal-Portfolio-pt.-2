c5ddb30a6c7aed9895b58b18a928fb9a41e0a7f8
asherz96 <peanutpie96@gmail.com>
2022-04-12 12:40:28 -0600

    pokeCode update

--- Pokemon/pokecode.js
+++ Pokemon/pokecode.js
@@ -7,20 +7,15 @@ const getAPIData = async (url) => {
     }
   }
 
-  const pokeGrid = document.querySelector('.pokeGrid')
-  
-  async function loadPokemon(offset, limit) {
-  const data =  await getAPIData('https://pokeapi.co/api/v2/pokemon/')
-  populatePokeGrid(data)
-}
+const pokeGrid = document.querySelector('.pokeGrid')
 
-function populatePokeGrid(pokemonArray) {
-  console.log(pokemonArray)
-// loop through array and populate individual pokemon cards
-for (const singlePokemon of pokemonArray.results){
-  populatePokeCard(singlePokemon)
-}
 
+  async function loadPokemon(offset = 0, limit = 25) {
+  const data = await getAPIData(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}}`)
+  for (const nameAndURL of data.results) {
+    const singlePokemon = await getAPIData(nameAndURL.url)
+    populatePokeCard(singlePokemon)
+  }
 }
 
 function populatePokeCard(pokemon) {
@@ -29,21 +24,22 @@ pokeScene.className = 'scene'
 const pokeCard = document.createElement( 'div' )
 pokeCard.className = 'card'
 pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
+
 // populate the front of the card
 
 pokeCard.appendChild(populateCardFront(pokemon))
+pokeCard.appendChild(populateCardBack(pokemon))
 pokeScene.appendChild(pokeCard)
 pokeGrid.appendChild(pokeScene)
-
 }
 
 function populateCardFront(pokemon) {
 const pokeFront = document.createElement( 'figure' )
-pokeFront.className = 'cardface' 
+pokeFront.className = 'cardFace Front' 
 const pokeImg = document.createElement( 'img' )
 pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
 const pokeCaption = document.createElement( 'figcaption' )
-pokeCaption.textContent = 'pokemon.name'
+pokeCaption.textContent = pokemon.name
 
 pokeFront.appendChild(pokeImg)
 pokeFront.appendChild(pokeCaption)
@@ -52,7 +48,23 @@ return pokeFront
 
 
 function populateCardBack(pokemon){
+const pokeBack = document.createElement('div')
+pokeBack.className = 'cardFace Back'
+const label = document.createElement('h4')
+label.textContent = 'Abilities'
+pokeBack.appendChild(label)
 
+const abilityList = document.createElement('ul')
+pokemon.abilities.forEach((abilityItem) => {
+  const listItem = document.createElement('li')
+  listItem.textContent = abilityItem.ability.name
+  abilityList.appendChild(listItem)
+
+
+})
+
+
+return pokeBack
 }
 
-loadPokemon(0,0)
+loadPokemon(800, 300)
